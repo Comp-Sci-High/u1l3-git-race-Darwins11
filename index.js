7a52418600286663e60d5a730ed960081aa1eb1c
Darwins11 <darwins.branche26@compscihigh.org>
2024-09-18 15:16:51 +0000

    Print Answer

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ Little Bites
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
-
+Sleepy Hollow
 // Stage and commit this change with an appropriate message
 // Go to more.js
